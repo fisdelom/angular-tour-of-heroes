d0b231439a7905ab1a1e00b542fbf0fec0edf4fc
fisdelom <fisdelom@gmail.com>
2023-08-30 16:06:25 +0100

    Get data foom server and search data

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -15,7 +15,8 @@ export class DashboardComponent implements OnInit {
     this.getHeroes();
   }
 getHeroes() : void{
-   this.heroService.getHeroes().subscribe(heroes =>this.heroes = heroes.slice(1, 5));
+   this.heroService.getHeroes()
+   .subscribe(heroes =>this.heroes = heroes.slice(1, 5));
 }
 
 }
